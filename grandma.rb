09f003b7bbac9c6f1a4a616010919fe6ff450120
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-29 03:18:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(greeting)  
   if greeting == greeting.upcase
     "NO, NOT SINCE 1938!"
-  elsif greeting == "I LOVE YOU GRANDMA!")
+  elsif greeting == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
     "HUH?! SPEAK UP, SONNY!"
